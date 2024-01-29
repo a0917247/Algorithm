73ce72e1444f3a68c22b87635489831a1a02feab
shirasawa1212 <a0917243@gmail.com>
2024-01-29 18:30:17 +0800

    dfs

---
+++ Algorithm/Algorithm/BinaryTree.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Algorithm
+{
+    public class TreeNode
+    {
+        public int? Value { get; set; }
+        public TreeNode? Right { get; set; }
+        public TreeNode? Left { get; set; }
+        /// <summary>
+        /// Node : BinaryTree Node
+        /// </summary>
+        /// <param name="value"></param>
+
+        //Init 並 定義預設null
+        public TreeNode(int? value) 
+        {
+            Value = value;
+            Right = null;
+            Left = null;
+        }
+    }
+    public class BinaryTree
+    {
+        public TreeNode? Root { get; set; }
+        public BinaryTree() 
+        {
+            Root = null;
+        }
+
+        //遍歷每個節點，依序從左至右，到節點就印出來
+        private void PreorderTraversal(TreeNode node)
+        {
+            if (node != null) 
+            {
+                Console.WriteLine(node.Value + " ");
+                PreorderTraversal(node.Left);
+                PreorderTraversal(node.Right);
+            }
+        }
+
+        private void InorderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.Left);
+                Console.WriteLine(node.Value + " ");
+                InorderTraversal(node.Right);
+            }
+        }
+
+        private void ProrderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                ProrderTraversal(node.Left);
+                ProrderTraversal(node.Right);
+                Console.WriteLine(node.Value + " ");
+            }
+        }
+
+        //前序
+        public void Preorder()
+        {
+            PreorderTraversal(Root);
+        }
+
+        //中序
+        public void Inorder()
+        {
+            InorderTraversal(Root);
+        }
+
+        //後序
+        public void Prorder()
+        {
+            ProrderTraversal(Root);
+        }
+
+        public void Print()
+        {
+            if (Root.Value == null)
+            {
+                Console.WriteLine("此樹沒有節點");
+            }
+            else
+            {
+                Console.WriteLine($"前序是 : ");
+                Preorder();
+                Console.WriteLine($"中序是 : ");
+                Inorder();
+                Console.WriteLine($"後序是 : ");
+                Prorder();
+            }
+        }
+    }
+}
--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -4,36 +4,37 @@ using Algorithm.Extension;
 namespace algorithm;
 public class Program
 {
-    static void Main(string[] args)
-    {
-        int[] ints = { 1, 4, 0, 1, 9, 66, 7 };
-        int[] twoInt = { 2, 4, 2, 1, 5, 4, 6, 5, 6, 2 };
-        MergeSort code = new MergeSort();
-        //SelectionSort code = new SelectionSort();
-        //int TestTime = 1000000;
-        //int maxSize = 100;
-        //bool Success = true;
-        //for (int i = 0; i < TestTime; i++)
-        //{
-        //    int[] arr1 = RandomArray(maxSize);
-        //    int[] arr2 = arr1.CopyArray();
-        //    code.Selectionsort(arr1);
-        //    comparator(arr2);
-        //    if (!arr1.Equal(arr2))
-        //    {
-        //        Success = false;
-        //        break;
-        //    }
-        //}
-        //Console.WriteLine(Success ? "測試成功" : "測試有誤");
-        //if (!Success)
-        //{
-        //    int[] arr = RandomArray(maxSize);
-        //    Print(arr);
-        //    code.Selectionsort(arr);
-        //    Print(arr);
-        //}
-    }
+    // 排序法
+    //static void Main(string[] args)
+    //{
+    //    int[] ints = { 1, 4, 0, 1, 9, 66, 7 };
+    //    int[] twoInt = { 2, 4, 2, 1, 5, 4, 6, 5, 6, 2 };
+    //    MergeSort code = new MergeSort();
+    //    //SelectionSort code = new SelectionSort();
+    //    //int TestTime = 1000000;
+    //    //int maxSize = 100;
+    //    //bool Success = true;
+    //    //for (int i = 0; i < TestTime; i++)
+    //    //{
+    //    //    int[] arr1 = RandomArray(maxSize);
+    //    //    int[] arr2 = arr1.CopyArray();
+    //    //    code.Selectionsort(arr1);
+    //    //    comparator(arr2);
+    //    //    if (!arr1.Equal(arr2))
+    //    //    {
+    //    //        Success = false;
+    //    //        break;
+    //    //    }
+    //    //}
+    //    //Console.WriteLine(Success ? "測試成功" : "測試有誤");
+    //    //if (!Success)
+    //    //{
+    //    //    int[] arr = RandomArray(maxSize);
+    //    //    Print(arr);
+    //    //    code.Selectionsort(arr);
+    //    //    Print(arr);
+    //    //}
+    //}
     public static void Print(int[] arr)
     {
         for (int i = 0; i < arr.Length; i++)
@@ -56,5 +57,13 @@ public class Program
     public static void comparator(int[] arr)
     {
         Array.Sort(arr);
+    }
+
+    static void Main(string[] args) 
+    {
+        BinaryTree binarytree = new BinaryTree();
+        binarytree.Root = new TreeNode(null);
+        Console.WriteLine(binarytree.Root.Value);
+        binarytree.Print();
     }
 }
